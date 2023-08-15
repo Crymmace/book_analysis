2af07d6732ac740b5fe7af72b82f4ea63084e073
Crystal Mace <crymace@outlook.com>
2023-08-15 11:12:51 +0200

    Implement mood determination by chapter

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import re
 import nltk
 from nltk.corpus import stopwords
+from nltk.sentiment import SentimentIntensityAnalyzer
 
 with open("miracle_in_the_andes.txt", "r") as file:
     book = file.read()
@@ -31,6 +32,19 @@ for count, word in d_list:
     if word not in english_stopwords:
         filtered_words.append((word, count))
 
+# Determine most positive/ negative chapters
 
+analyzer = SentimentIntensityAnalyzer()
 
+pattern2 = re.compile("Chapter [0-9]+")
+chapters = re.split(pattern2, book)
+chapters = chapters[1:]
+
+for chapter in chapters:
+    scores = analyzer.polarity_scores(chapter)
+
+    if scores["pos"] > scores["neg"]:
+        print("This chapter is a positive text.")
+    else:
+        print("This chapter is a negative text.")
 
